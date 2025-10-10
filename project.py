9ed1a0b7f5443fff222dafe6da27cedb0c8cb99b
Iskandar <karamov.07@bk.ru>
2025-10-10 16:10:00 +0500

    smth

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 def f(trg, nums):
     checked = []
+    result = []
     flag1 = 1
     flag2 = 1
     if len(nums) > 1:
@@ -8,11 +9,11 @@ def f(trg, nums):
                 if i not in checked and j not in checked:
                     if nums[i] + nums[j] == trg:
                         if flag1:
-                            print('Minimum:', i, j)
+                            result.append(i)
+                            result.append(j)
                             checked.append(i)
                             checked.append(j)
                             flag1 = 0
-                            print('Others:', end=' ')
                             flag2 = 0
                         else:
                             print(f'({i}, {j})', end=' ')
@@ -22,11 +23,44 @@ def f(trg, nums):
             print('None')
     else:
         print('The list must contain more than 1 element!')
-
+    return result
 
 try:
     nums = [int(i) for i in input('Введите список (через пробел): ').split()]
     trg = int(input("Введите искомую сумму: "))
-    f(trg, nums)
+    print(f(trg, nums))
 except ValueError:
     print('That was no valid number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import unittest
+
+
+# Тесты
+class TestMath(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(f(9, [2, 7, 11, 15]), [0, 1])
+
+    def test2(self):
+        self.assertEqual(f(6, [3, 2, 4]), [1, 2])
+
+    def test3(self):
+        self.assertEqual(f(6, [3, 3]), [0, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
