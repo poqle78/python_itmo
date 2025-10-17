81de755047e34cd31fc8cc94c512fbc5b5aab234
Iskandar <karamov.07@bk.ru>
2025-10-17 17:27:59 +0300

    smth

---
+++ lab6/rec_bin_tree.py
@@ -0,0 +1,19 @@
+def recursive_gen_bin_tree(root: int, height: int) -> dict:
+    """
+    Функция создания бинарного дерева
+
+    :param height: Глубина дерева. Натуральное число.
+    :param root: Корень дерева. Принимает только целое значение.
+    :return: Возвращает дерево в виде словаря.
+    """
+
+    # Выход из рекурсии
+    if height == 1:
+        return {root: []}
+
+    # Создание левого и правого потомка рекурсивным способом.
+    left_leaf = recursive_gen_bin_tree(root ** 2, height - 1)
+    right_leaf = recursive_gen_bin_tree(root - 2, height - 1)
+
+    return {root: [left_leaf, right_leaf]}
+
